10dc7627525d2797c1190ef0012a9a9215e685e8
Abdullah Al Mamun <abdullahgodboy@gmail.com>
2024-12-30 20:45:38 +0600

    handle type error in emailSection

--- app/components/emailsection/index.tsx
+++ app/components/emailsection/index.tsx
@@ -1,38 +1,40 @@
 "use client"
+
 import { useState } from 'react'
 import Input from '../form/input/Input'
-import { UserType } from '@/app/type'
-import Button from '../button/Button'
 
 
 const EmailSection = () => {
 
-    
 
-    const [userInfo, setUserInfo] = useState<Partial<UserType>>({
-        userEmail: "",
-    })
+
+    const [emailInfo, setEmailInfo] = useState<string>("")
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setUserInfo({
-            ...userInfo,
-            [e.target.name]: e.target.value,
-        })
+        setEmailInfo(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        console.log(emailInfo)
     }
 
     return (
-        <div className='min-w-full flex justify-between items-center h-[400px] px-4 lg:px-24 gap-4'>
+        <div className='min-w-full flex justify-between items-center h-[400px] px-4 lg:px-24 gap-6'>
             <div className='w-full'>
 
-                <Input heading="Please Enter your Email."
-                    name="userEmail" type="email"
-                    number={1} placeholder="Jhon@gmail.com"
-                    userValue={userInfo.userEmail}
-                    onChange={(e) => handleChange(e)}
-                />
+                <form onSubmit={handleSubmit}>
+
+
+                    <Input heading="Please Enter your Email."
+                        name="userEmail" type="email"
+                        number={1} placeholder="Jhon@gmail.com"
+                        userValue={emailInfo}
+                        onChange={(e) => handleChange(e)}
+                    />
+                </form>
             </div>
             <div className='p-2 rounded-2xl'>
-                <Button background="#6065ff" contact content="Submit" />
+                <button className='text-nowrap rounded-lg bg-white px-6 py-4 text-xl font-bold hover:bg-black hover:border hover:text-white'>Send us</button>
             </div>
         </div>
     )
